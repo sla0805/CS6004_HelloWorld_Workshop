761f537a5e2704d3a882294a4e56359a8896d0b3
sla0805 <suleiaung0805@gmail.com>
2025-02-26 16:35:19 +0700

    Modify to ask for the user's name and multiple greetings.

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,9 +6,23 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey(); // Wait for user input before closing
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("Hello, World!");
+                Console.Write("Please enter your username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine($"Hello, {username}.");
+                Console.WriteLine("Do you want to exit the program? (y/n)");
+                string response = Console.ReadLine();
+
+                if (response == "y")
+                {
+                    loop = false;
+                }
+            }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(); // Wait for user input before closing
         }
     }
 }

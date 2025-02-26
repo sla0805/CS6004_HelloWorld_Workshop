0e97e1be54200696d6f18a09ab5d2dd7d76a83f7
sla0805 <suleiaung0805@gmail.com>
2025-02-26 15:56:25 +0700

    Initial commit Hello World Project

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); // Wait for user input before closing
+        }
+    }
+}
